57889cb7fd2ecc6e97187f0b6eed1b1e973ba191
Hector <hectortilla6@gmail.com>
2019-04-11 17:26:11 +0200

    Added Usuario Bean

--- TournamentsWeb/src/java/Usuario.java
+++ TournamentsWeb/src/java/Usuario.java
@@ -1,3 +1,6 @@
+
+import java.util.Date;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,5 +16,14 @@ public class Usuario {
     private int id;
     private String nombre;
     private String email;
+    private Date fechaCreacion; 
     
+    Usuario(){
+    Algo();
+    }
+    
+    public void Algo(){
+        this.id=1;
+        int t=id;
+    }
 }
