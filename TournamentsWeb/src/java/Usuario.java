7079adc4800c7e25de7ac9dec1378db535893bbe
Hector <hectortilla6@gmail.com>
2019-04-11 17:25:55 +0200

    cambio de sitio+ java

---
+++ TournamentsWeb/src/java/Usuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hector
+ */
+public class Usuario {
+    
+    private int id;
+    private String nombre;
+    private String email;
+    
+}
---
+++ TournamentsWeb/web/competiciones.js
@@ -0,0 +1,41 @@
+function lol () {
+	var torneo1="<table id="ultimostorneos">
+		<tr>
+			<td><i class="fa fa-instagram"></i></td>
+			<td>31/03/2019 16:00</td>
+			<td><img class="icono" src="img/lolico.png"></img></td>
+			<td><a href="torneo1.html">#Torneo Inagural de League of Legends</a></td>
+			<td><span> 9 equipos </span></td>
+		</tr>
+		<tr>
+			<td><i class="fa fa-instagram"></i></td>
+			<td>31/03/2019 16:00</td>
+			<td><img class="icono" src="img/lolico.png"></img></td>
+			<td><a href="torneo1.html">#Torneo Inagural de League of Legends</a></td>
+			<td><span> 9 equipos </span></td>
+		</tr>
+		<tr>
+			<td><i class="fa fa-instagram"></i></td>
+			<td>31/03/2019 16:00</td>
+			<td><img class="icono" src="img/lolico.png"></img></td>
+			<td><a href="torneo1.html">#Torneo Inagural de League of Legends</a></td>
+			<td><span> 9 equipos </span></td>
+		</tr>
+		<tr>
+			<td><i class="fa fa-instagram"></i></td>
+			<td>31/03/2019 16:00</td>
+			<td><img class="icono" src="img/lolico.png"></img></td>
+			<td><a href="torneo1.html">#Torneo Inagural de League of Legends</a></td>
+			<td><span> 9 equipos </span></td>
+		</tr>
+		<tr>
+			<td><i class="fa fa-instagram"></i></td>
+			<td>31/03/2019 16:00</td>
+			<td><img class="icono" src="img/lolico.png"></img></td>
+			<td><a href="torneo1.html">#Torneo Inagural de League of Legends</a></td>
+			<td><span> 9 equipos </span></td>
+		</tr>
+	
+	</table>";
+	document.getElementById("torneo1").innerHTML=torneo1;
+}
