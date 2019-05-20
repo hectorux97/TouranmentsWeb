a311615c455e180d0eb1d1d5e7142c1e76a442da
angel <angel.angulo95@gmail.com>
2019-05-21 00:15:34 +0200

    controlador aprobar

---
+++ TournamentsWeb/src/java/Controlador/AprobarPartidos.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import DAO.PartidoDAO;
+import DAO.TorneoDAO;
+import Gestor.GestorPartidos;
+import Gestor.GestorTorneos;
+import beans.Juego;
+import beans.Partido;
+import beans.Torneo;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author angel
+ */
+@WebServlet(urlPatterns = {"/Controlador/AprobarPartidos"})
+public class AprobarPartidos extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            int idpartido =Integer.parseInt(request.getParameter("idPartido"));
+        PartidoDAO nuevopartido = new PartidoDAO();
+            Partido partido = nuevopartido.GetPartido(idpartido);
+        
+        GestorPartidos gestor = new GestorPartidos();
+        boolean correcto;
+        if (partido.getEstado()==2) {
+            correcto=gestor.ValidarPartido(idpartido);
+        }
+        else {
+            correcto=false;
+        }
+        if (correcto) {
+
+            response.sendRedirect("/AdministrarTorneos.jsp?re="+(partido.getEstado()==2?"NO":"SI"));
+
+        } else {
+            response.sendRedirect("/Register.jsp?error=PASSWORD_USER_ERROR");
+        }
+       
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
